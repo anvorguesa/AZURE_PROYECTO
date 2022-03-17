b20000ec38d463fdb8668f519acc455a447ee7f5
anvorguesa <srliposa@gmail.com>
2022-03-17 18:45:50 -0500

    Estructura Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZURE_PROYECTO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
